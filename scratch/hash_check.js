f6386f418dfe67514b42238bb8af09c57547d1bd
Abdelmounaim <aqa.sports.academy@gmail.com>
2026-06-11 20:11:54 +0200

    cleanup: remove deployment scripts and scratch folder

--- scratch/hash_check.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-const pwd = 'AqaSports2026!';
-const hash = crypto.createHash('sha256').update(pwd).digest('hex');
-console.log('Password hash of AqaSports2026!:');
-console.log(hash);
